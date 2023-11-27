eaa3ef63f906491102b1ec2f18cc8086febd5a3c
Ivona Cvija <123738999+IvonaCvija@users.noreply.github.com>
2023-11-27 17:42:28 +0000

    Update server.js

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -3,7 +3,10 @@ const app = express()
 const port = 4000;
 const cors = require('cors');
 
+
+// Enable Cross-Origin Resource Sharing (CORS)
 app.use(cors());
+// Middleware to set headers for CORS
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
@@ -13,27 +16,29 @@ app.use(function (req, res, next) {
 });
 
 const bodyParser = require('body-parser');
+// Parse incoming requests with JSON and URL-encoded payloads
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// getting-started.js
+// MongoDB connection setup
 const mongoose = require('mongoose');
 
 main().catch(err => console.log(err));
 
+// Connect to the MongoDB database
 async function main() {
     // username:password
     await mongoose.connect('mongodb+srv://user:user2023@cluster0.1kojhqr.mongodb.net/?retryWrites=true&w=majority');
 
     // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled, 
 }
-
+// Book schema definition
 const bookSchema = new mongoose.Schema({
     title:String,
     cover:String,
     author:String
 })
-    
+// Create a model based on the book schema   
 const bookModel = mongoose.model(`books`, bookSchema)
 
 //updating existing data using id
@@ -45,9 +50,10 @@ app.put('/api/book/:id', async (req,res)=>{
     let book = await bookModel.findByIdAndUpdate(req.params.id, req.body, {new:true});
     res.send(book);
 })
-
+// Create a new book entry
 app.post('/api/book', (req, res) => {
     console.log(req.body);
+    // Create a new book using the data from the request body
     bookModel.create({
         title:req.body.title,
         cover:req.body.cover,
@@ -61,18 +67,18 @@ app.post('/api/book', (req, res) => {
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
-
+// Get a specific book by ID
 app.get(`/api/book/:id`, async (req, res)=>{
     console.log(req.params.id);
-
+    // Find a book by its ID
     let book = await bookModel.findById({_id:req.params.id})
     res.send(book);
 })
-
+// Listen on the specified port
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
-
+// Get all books
 app.get('/api/books', async (req, res) => {
     
     let books = await bookModel.find({});
